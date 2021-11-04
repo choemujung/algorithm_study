d701f4187e44a4da61c97565fd8e541e78cee462
choemujung <mnw0507@naver.com>
2021-11-04 17:34:15 +0900

    :~27

--- q_26.py
+++ q_26.py
@@ -5,60 +5,64 @@ class ListNode:
         self.right = right
 
 class MyCircularDeque:
-
-
     def __init__(self, k: int):
-        self.head = self.tail = ListNode()
+        self.head, self.tail = ListNode(), ListNode()
         self.k, self.len = k, 0
+        self.head.right, self.tail.left = self.tail, self.head
+
+    '''
+    node <-> new <-> node.left 이렇게 사이게 끼우는 형식
+    node의 오른쪽에 new를 연결
+    따라서 insertFront: add(head), insertLast: add(tail.left)
+    '''
+
+    def _add(self, node: ListNode, new: ListNode):
+        n = node.right
+        node.right = new
+        new.left, new.right = node, n
+        n.left = new
+
+    def _del(self, node: ListNode):  # node의 오른쪽 노드를 삭제. 따라서 head 삭제: del(head), tail 삭제: del(tail.left.left)
+        n = node.right.right
+        node.right = n
+        n.left = node
+
     def insertFront(self, value: int) -> bool:
         if self.k == self.len:
             return False
-        if self.len == 0:
-            self.head.val = value
-        else:
-            self.head.left = ListNode(val=value, right=self.head)
-            self.head = self.head.left
+        self._add(self.head, ListNode(value))
         self.len += 1
         return True
+
     def insertLast(self, value: int) -> bool:
         if self.k == self.len:
             return False
-        if self.len == 0:
-            self.tail.val = value
-        else:
-            self.tail.right = ListNode(val=value, left=self.tail)
-            self.tail = self.tail.right
+        self._add(self.tail.left, ListNode(value))
         self.len += 1
         return True
+
     def deleteFront(self) -> bool:
         if self.len == 0:
             return False
-        self.head, self.head.right = self.head.right, None
-        self.head.left = None
+        self._del(self.head)
         self.len -= 1
         return True
+
     def deleteLast(self) -> bool:
         if self.len == 0:
             return False
-        self.tail, self.tail.left = self.tail.left, None
-        self.tail.right = None
+        self._del(self.tail.left.left)
         self.len -= 1
         return True
+
     def getFront(self) -> int:
-        if self.len == 0:
-            return -1
-        return self.head.val
+        return self.head.right.val if self.len else -1
+
     def getRear(self) -> int:
-        if self.len == 0:
-            return -1
-        return self.tail.val
+        return self.tail.left.val if self.len else -1
+
     def isEmpty(self) -> bool:
-        if self.len == 0:
-            return True
-        else:
-            return False
+        return self.len == 0
+
     def isFull(self) -> bool:
-        if self.len == self.k:
-            return True
-        else:
-            return False
+        return self.k == self.len
